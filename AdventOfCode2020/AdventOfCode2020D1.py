a3a470e500bc61688f2e5f5c74ef7e2d9a2dc23b
SolidTitanium <g.felix.e.spin@gmail.com>
2021-07-10 05:12:36 -0700

    Part 2 od D1 of AOC2020

--- AdventOfCode2020/AdventOfCode2020D1.py
+++ AdventOfCode2020/AdventOfCode2020D1.py
@@ -1,9 +1,9 @@
 import itertools as it
 
-with open("AdventOfCode2020\AOC2020Input\AOC2020D1Input.txt", "r") as f:
+with open("AdventOfCode2020\AOC2020Input\AOC2020D1P2Input.txt", "r") as f:
     InputList = f.readlines()
 
 ParsedInputList = list(map(lambda x: int(x.strip()), InputList))
-CartesianMess = it.product(ParsedInputList, ParsedInputList)
-Solutions = [i*j for i, j in CartesianMess if i + j == 2020]
+CartesianMess = it.combinations(ParsedInputList, 3)
+Solutions = [i*j*k for i, j, k in CartesianMess if i + j + k == 2020]
 print(Solutions)
